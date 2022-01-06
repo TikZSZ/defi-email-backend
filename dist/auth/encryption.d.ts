6d5f9e8d36bdc4ba662665b0f7f0b1b5865363bb
TikZSZ <repackergame@gmail.com>
2022-01-06 07:17:42 +0530

    initial commit

--- dist/auth/encryption.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum CipherAlgorithm {
-    Aes128Ctr = "AES-128-CTR",
-    Aes128Cbc = "AES-128-CBC"
-}
-export declare function createCipheriv(algorithm: CipherAlgorithm, key: Uint8Array, iv: Uint8Array | null, data: Uint8Array): Promise<Uint8Array>;
-export declare function createDecipheriv(algorithm: CipherAlgorithm, key: Uint8Array, iv: Uint8Array | null, data: Uint8Array): Promise<Uint8Array>;
